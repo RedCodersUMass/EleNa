3b0e24148e9f4032f26b62f28e626279a5e2b7fa
Mrinal Tak <mrinaltak@gmail.com>
2021-04-29 15:15:36 -0400

    added fetching coordinates from address

--- src/driver.py
+++ src/driver.py
@@ -5,6 +5,7 @@ from src.controller.AStarController import *
 from src.controller.DijkstraController import *
 from src.constants.constants import *
 import json
+from geopy.geocoders import Nominatim
 
 ACCESS_KEY = 'pk.eyJ1IjoibXRhayIsImEiOiJja25wNmdyMTMxYm9tMm5wZTlha2lhcmFnIn0.JsFh89MfCIDr32o-1OHmdA'
 static_folder = "./view/static"
@@ -22,7 +23,6 @@ def client():
         ACCESS_KEY=ACCESS_KEY
     )
 
-
 @app.route('/path_via_pointers', methods=['POST'])
 def get_route():
     json_output = request.get_json(force=True)
@@ -32,7 +32,40 @@ def get_route():
     origin_point = (origin_coords['lat'], origin_coords['lng'])
     destination_point = (destination_coords['lat'], destination_coords['lng'])
     path_limit = float(json_output['elevation_percent'])
-    elevation_strategy = MAXIMIZE #json_output['min_max'] #TODO
+    elevation_strategy = json_output['min_max']
+    algorithm = json_output['algorithm']
+    model = Model()
+    view = View()
+    model.register_observer(view)
+    if algorithm == "AStar":
+        controller = AStarController()
+    else:
+        controller = DijkstraController()
+    controller.set_model(model)
+    controller.set_start_point(origin_point)
+    controller.set_end_point(destination_point)
+    controller.set_path_limit(path_limit)
+    controller.set_elevation_strategy(elevation_strategy)
+    controller.manipulate_model()
+    return view.get_output_json()
+
+def convert_address_to_coordinates(location_name):
+    coordinates = Nominatim(user_agent="chrome").geocode(location_name)
+    if coordinates is None:
+        return None, None
+    else:
+        return location.latitude, location.longitude
+
+@app.route('/path_via_address', methods=['POST'])
+def get_routes_via_address():
+    json_output = request.get_json(force=True)
+    print('Request - ', json_output)
+    start_address = json.loads(json_output['text_origin_address'])
+    end_address = json.loads(json_output['text_dest_address'])
+    origin_point = convert_address_to_coordinates(start_address)
+    destination_point = convert_address_to_coordinates(end_address)
+    path_limit = float(json_output['elevation_percent'])
+    elevation_strategy = MAXIMIZE  # json_output['min_max'] #TODO
     algorithm = json_output['algorithm']
     model = Model()
     view = View()
--- src/model/algorithms/algorithms_interface.py
+++ src/model/algorithms/algorithms_interface.py
@@ -3,6 +3,9 @@ from abc import ABC, abstractmethod
 
 class AlgorithmsInterface:
 
+    def __init__(self):
+        pass
+
     @abstractmethod
     def shortest_route(self):
         pass
