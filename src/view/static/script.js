8bd152e9acc208a343d18e0d6e67e55cad0dd1d0
= <=>
2021-05-02 00:41:18 -0400

    FIxed minor bug

--- src/view/static/script.js
+++ src/view/static/script.js
@@ -69,7 +69,7 @@ function resetOutputs() {
     document.getElementById("shortestDistance").innerHTML = "";
     document.getElementById("elevationDistance").innerHTML = "";
     document.getElementById("gainShort").innerHTML = "";
-    document.getElementById("gainDrop").innerHTML = "";
+    document.getElementById("dropShort").innerHTML = "";
     document.getElementById("elev_path_gain").innerHTML = "";
     document.getElementById("elev_path_drop").innerHTML = "";
 
@@ -220,12 +220,12 @@ function plotRoute(data, endpoint) {
 }
 
 function updateOutputs(data) {
-    document.getElementById("shortestDistance").innerHTML = data["shortDist"].toFixed(4) + "meters";
-    document.getElementById("elevationDistance").innerHTML = data["elev_path_dist"].toFixed(4) + "meters";
-    document.getElementById("gainShort").innerHTML = data["gainShort"].toFixed(4) + "meters";
-    document.getElementById("dropShort").innerHTML = data["dropShort"].toFixed(4) + "meters";
-    document.getElementById("elev_path_gain").innerHTML = data["elev_path_gain"].toFixed(4) + "meters";
-    document.getElementById("elev_path_drop").innerHTML = data["elev_path_drop"].toFixed(4) + "meters";
+    document.getElementById("shortestDistance").innerHTML = data["shortDist"].toFixed(4) + " meters";
+    document.getElementById("elevationDistance").innerHTML = data["elev_path_dist"].toFixed(4) + " meters";
+    document.getElementById("gainShort").innerHTML = data["gainShort"].toFixed(4) + " meters";
+    document.getElementById("dropShort").innerHTML = data["dropShort"].toFixed(4) + " meters";
+    document.getElementById("elev_path_gain").innerHTML = data["elev_path_gain"].toFixed(4) + " meters";
+    document.getElementById("elev_path_drop").innerHTML = data["elev_path_drop"].toFixed(4) + " meters";
 }
 
 mapboxgl.accessToken = access_key;
