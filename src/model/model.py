8b25337110081c1005bc66e77a931671211d86f1
Sirisha Annamraju <sirisha.annamraju91@gmail.com>
2021-05-08 14:58:07 -0400

    Added documentation for model.py

--- src/model/model.py
+++ src/model/model.py
@@ -8,9 +8,15 @@ from src.model.DijkstraRoute import DijkstraRoute
 from src.model.AStarRoute import AstarRoute
 from src.model.utils import get_path_weight
 
-
 class Model:
+    """
+    This class initializes critical parameters such the graph,the algorithm,the path_limit  etc.It contains methods to register the
+    observer,set the algorithm,set the algorithm object,print the route information etc and also it notifies the observers.
+    """
     def __init__(self):
+        """
+        This method initializes the  route and algorithm attributes
+        """
         self.mapbox_api_key = None
         self.graph = None
         self.shortest_route_object = None
@@ -24,23 +30,77 @@ class Model:
         self.elevation_strategy = None
 
     def register_observer(self, observer):
+        """
+        This method registers the observer.
+
+        Args:
+            observer:
+
+        Returns:
+
+        """
         self.observer = observer
 
     def set_algorithm(self, algorithm):
+        """
+        This is the setter method for the algorithm.
+
+        Args:
+            algorithm:
+
+        Returns:
+
+        """
+
         self.algorithm = algorithm
 
     def get_mapbox_api(self):
+        """
+        This is the method for fetching the mapbox_api key.
+
+        Returns:
+        The mapbox_api_key.
+
+        """
         return self.mapbox_api_key
 
     def set_mapbox_api(self, api_key):
+        """
+        This is the setter method for setting the value of the mapbox_api key.
+
+        Args:
+            api_key:
+
+        Returns:
+
+        """
+
         self.mapbox_api_key = api_key
 
     def set_shortest_route_object(self, coord_start, coord_end):
+        """
+        This method sets the shortest path based on the starting and ending coordinates.
+
+        Args:
+            coord_start:
+            coord_end:
+
+        Returns:
+
+        """
         self.graph = GraphGenerator().generate_elevation_graph(coord_end)
         self.shortest_route_object = ShortestRoute(self.graph)
         self.shortest_path_information = self.shortest_route_object.get_shortest_route(coord_start, coord_end)
 
     def set_algorithm_object(self):
+        """
+        This method sets the algorithm object by passing the graph,distance,path_limit,elevation strategy,starting node,ending node
+        and gain values to the algorithm.
+
+        Returns:
+
+        """
+
         self.algorithm_object = self.algorithm(self.graph,
                                                self.shortest_path_information.get_distance(),
                                                self.path_limit,
@@ -50,6 +110,14 @@ class Model:
                                                self.shortest_path_information.get_total_gain())
 
     def print_route_information(self, route):
+        """
+        This method prints the route information to the terminal.
+        Args:
+            route:
+
+        Returns:
+
+        """
         print("------------------------------------------------")
         print("Algorithm Strategy:" + route.get_algorithm_name())
         print("Total Distance: " + str(route.get_distance()))
@@ -57,6 +125,17 @@ class Model:
         print("Elevation Drop: " + str(route.get_total_drop()))
 
     def generate_paths(self, origin, destination, path_limit, elevation_strategy):
+        """
+        This method generates the shortest path based on the elevation strategy and the path limit specified.
+        Args:
+            origin:
+            destination:
+            path_limit:
+            elevation_strategy:
+
+        Returns:
+
+        """
         # calculate shortest path
         self.set_shortest_route_object(origin, destination)
         self.print_route_information(self.shortest_path_information)
